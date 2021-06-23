b45303a9631a55acee2e2fac2c8ac87131cc77d2
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-23 11:49:22 -0600

    Router setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimerComponent } from './components/timer/timer.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'settings', component: SettingsComponent },
+  { path: '', component: TimerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
