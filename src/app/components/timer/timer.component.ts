22cdc4bc5e6e9ceade4872959d6be2e37b0892c0
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-23 21:38:47 -0600

    Timer component setup

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { formatTime } from './timer.helpers';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  styleUrls: ['./timer.component.scss'],
+  host: {
+    class: 'timer-comp',
+  },
 })
 export class TimerComponent implements OnInit {
+  isPlaying: boolean = false;
+  isWork: boolean = true;
 
-  constructor() { }
+  private settingsWorkTime = 5736;
+  private settingsPlayTime = 1800;
+  currentTime = this.isWork ? this.settingsWorkTime : this.settingsPlayTime;
 
-  ngOnInit(): void {
+  formatTime = formatTime;
+  private runningInterval: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  startTimer(): void {
+    this.runningInterval = setInterval(() => this.currentTime--, 1000);
   }
 
+  handleReset() {
+    this.isPlaying = false;
+    this.currentTime = this.isWork
+      ? this.settingsWorkTime
+      : this.settingsPlayTime;
+    clearInterval(this.runningInterval);
+  }
+
+  handlePlayPause() {
+    if (!this.isPlaying) this.startTimer();
+    if (this.isPlaying) clearInterval(this.runningInterval);
+
+    this.isPlaying = !this.isPlaying;
+  }
+
+  handleSkip() {
+    this.isWork = !this.isWork;
+    this.handleReset();
+  }
 }
